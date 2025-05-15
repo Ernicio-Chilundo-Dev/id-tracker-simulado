3a019a887318cd9547ea986c04745d9344999122
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-05-15 13:20:29 +0200

    logica do programa feita com sucesso ,  contendo apenas uma funcao

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ const dados = [
     {nome:"Djen da Lurdes Sosuke", bi:"20202020MB02", nuit:"987654321", telefone:"+258870000002", cidade:"Beira"}
 ];
 
+function buscarContato(){
+    const bi = document.getElementById("bi").value.trim();
+    const nuit = document.getElementById('nuit').value.trim();
+    const resultadoDiv = document.getElementById('resultado');
+
+    const contato = dados.find(p=>p.bi==bi && p.nuit == nuit);
+
+    if(contato){
+        resultadoDiv.innerHTML= `
+            <p><strong>Nome:</strong> ${contato.nome}</p>
+            <p><strong>Telefone:</strong> ${contato.telefone}</p>
+            <p><strong>Cidade:<strong> ${contato.cidade}
+        `;
+    }else{
+        resultadoDiv.innerHTML = `<p style ="color:red;">❌ Contato nao encontrado!`;
+    }
+
+}
