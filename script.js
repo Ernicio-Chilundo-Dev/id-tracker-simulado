bd684f69c8770bfe2e251909f488d28df3b39f34
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-05-15 12:54:09 +0200

    banco de dados criado com sucesso

---
+++ script.js
@@ -0,0 +1,5 @@
+const dados = [
+    {nome:"Ernicio Jermias Chilundo", bi:"10101010MB04", nuit:"123456789", telefone:"+258840000001", cidade:"Inhambane"},
+    {nome:"Djen da Lurdes Sosuke", bi:"20202020MB02", nuit:"987654321", telefone:"+258870000002", cidade:"Beira"}
+];
+
